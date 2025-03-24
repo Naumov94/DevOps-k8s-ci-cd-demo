8fdcdc861d927eca589eeba874036ba8c12d660e
Vadim <chester.kms@gmail.com>
2025-03-23 21:11:27 -0600

    Fix import error in Flask app
    Corrected 'from flask import flask' to 'from flask import Flask'.
    This resolves the import error and allows the app to run properly on localhost.

--- app/app.py
+++ app/app.py
@@ -1,11 +1,10 @@
-from flask import flask
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
-
 def home():
-  return "Hello from Flask inside Docker + Kubernetes!"
+    return "Hello from Flask inside Docker + Kubernetes!"
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
